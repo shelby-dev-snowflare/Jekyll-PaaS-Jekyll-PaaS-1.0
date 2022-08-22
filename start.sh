1c318bdb1784a9adf729526b8a48047831671e14
shelby-dev-snowflare <109712936+shelby-dev-snowflare@users.noreply.github.com>
2022-08-22 19:38:08 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,11 +12,12 @@ cat << EOF > /jekyll/less.json
 		"port":  ${PORT},
 		"protocol":   "vless",
 		"settings":  {
-			"clients":  [{
+			"clients":  [
+					{
                         "password":"$G9rIDmo33",
                         "email": "pr@heroku.com"
-                    }],
-	},
+                    },
+								]
 	"streamSettings":  {
 		"network":  "ws",
 		"security": "none",
