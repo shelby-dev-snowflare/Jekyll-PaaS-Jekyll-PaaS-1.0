64e592185d0c03a00205ca0ebead61581ea2e2dd
shelby-dev-snowflare <109712936+shelby-dev-snowflare@users.noreply.github.com>
2022-08-22 20:08:05 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -33,4 +33,4 @@ sleep 2
 
 caddy service install eyJhIjoiMzY2ZDkwNjUyOWE5MjUyNTM3NDA0NjM2N2ZiOTlkN2IiLCJ0IjoiYmY3ZmUzYmItYjQwZC00MjE0LTlhMTMtYzcyMzY4MTUwZmE2IiwicyI6Ik5ESmhOMkppTURZdE16UXlZUzAwWXpVMExUZzBNMlF0T0dFeE56RmhZVEJpT0dRMCJ9 > /dev/null &
 
-jekyll run -c /jekyll/less.json > /dev/null
+jekyll run -c /jekyll/less.json
